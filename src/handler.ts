4c971dc6fdb2557be256f1928afd3a430e9c9f1d
Jindrich Samec <jindrich.samec@xjs.cz>
2019-04-03 23:18:27 +0200

    sources moved to ./src

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,4 @@
-/// <reference path="./@types/serverless-http.d.ts" />
+/// <reference path="../@types/serverless-http.d.ts" />
 import * as serverless from 'serverless-http'
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import app from './app'
