231a5c68d0d87a00de5a9617e3538a84c45731e4
Jindrich Samec <jindrich.samec@xjs.cz>
2019-04-06 13:08:01 +0200

    unified app endpoint to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,12 @@ import * as serverless from 'serverless-http'
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import app from './app'
 
+const SERVICE_PORT = process.env.SERVICE_PORT || 3000
+
+if (!module.parent) {
+  app.listen(SERVICE_PORT)
+  console.info('Server runnning on port', SERVICE_PORT)
+}
+
 export const toggl: APIGatewayProxyHandler = serverless(app)
+
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app'
-
-const SERVICE_PORT = process.env.SERVICE_PORT || 3000
-
-if (!module.parent) {
-  app.listen(SERVICE_PORT)
-  console.info('Server runnning on port', SERVICE_PORT)
-}
